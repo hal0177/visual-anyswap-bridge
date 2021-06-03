7ed0330d4737166c2ae62350823d556959bc9cf6
Paddy Curé <paddy.cure@gmail.com>
2021-06-03 20:03:14 +0100

    dynamic from/to chain, token

--- src/Chains.js
+++ src/Chains.js
@@ -3,10 +3,26 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { motion, useAnimation } from "framer-motion";
 
+import anyswap from "./icons/anyswap.png";
+
 import fusion from "./icons/fusion.png";
 import bsc from "./icons/bsc.png";
-import polygon from "./icons/polygon.png";
 import ethereum from "./icons/ethereum.png";
+import fantom from "./icons/fantom.png";
+import huobi from "./icons/huobi.png";
+import polygon from "./icons/polygon.png";
+import xdai from "./icons/xdai.png";
+import avalanche from "./icons/avalanche.png";
+import harmony from "./icons/harmony.png";
+import bitcoin from "./icons/bitcoin.png";
+import litecoin from "./icons/litecoin.png";
+import blocknet from "./icons/blocknet.png";
+import colossusxt from "./icons/colossusxt.png";
+
+import any from "./icons/tokens/any.svg";
+import anybtc from "./icons/tokens/anybtc.png";
+import anyfsn from "./icons/tokens/anyfsn.png";
+import anyusdt from "./icons/tokens/anyusdt.png";
 
 const ChainsContainer = styled.div`
   position: absolute;
@@ -25,7 +41,7 @@ const Chains = styled.div`
   height: ${props => props.size}px;
   border-radius: 50%;
   background: rgb(38, 38, 38);
-  box-shadow: 0px 0px 15px rgb(30, 144, 255);
+  // box-shadow: 0px 0px 15px rgb(30, 144, 255);
 `
 
 const Crypto = styled.div`
@@ -39,123 +55,165 @@ const Crypto = styled.div`
   height: ${props => props.r}px;
   margin: 0;
   border-radius: 50%;
-  background: #222;
-  box-shadow: 1px 1px 5px rgb(30, 144, 255);
+  background: rgb(255, 255, 255);
+  box-shadow: 0px 0px 12px rgb(100, 98, 244);
 `
 
 const Icon = styled.img`
-  width: ${props => props.r}px;
-  height: ${props => props.r}px;
+  // width: ${props => props.r}px;
+  // height: ${props => props.r}px;
+  width: 70%;
+  height: 70%;
 `
 
-const Bridge = styled.div`
+const Bridge = styled.img`
   position: absolute;
   top: ${props => props.top}px;
   left: ${props => props.left}px;
   width: 50px;
   height: 50px;
-  border-radius: 50%;
-  background: white;
-  box-shadow: 0.5px 0.5px 5px rgb(30, 144, 255);
+  box-shadow: 0px 0px 12px rgb(100, 98, 244);
+  border-radius: 35%;
 `
 
-const Payload = styled(motion.div)`
-  position: absolute;
+const Payload = styled(motion.img)`
+  position: relative;
   width: 40px;
   height: 40px;
-  border-radius: 50%;
-  background: gold;
   z-index: 2;
 `
 
 
 const InterChains = () => {
 
-  const [ layout, setLayout ] = useState([]);
-  const [ bridgeTarget, setBridgeTarget ] = useState();
-  const [ targets, setTargets ] = useState([]);
+  const R = 300;
+  const N = 13;
+  const RESIZE = 0.5;
+  const r = Math.PI * R / N * RESIZE;
+  const BRIDGE = (R + r / 2) + 5;
 
-
-  const chains = [
+  const CHAINS = [
     fusion,
-    polygon,
-    ethereum,
     bsc,
-    fusion,
-    polygon,
     ethereum,
-    bsc
+    fantom,
+    huobi,
+    polygon,
+    xdai,
+    avalanche,
+    harmony,
+    bitcoin,
+    litecoin,
+    blocknet,
+    colossusxt
   ]
 
+  const TOKENS = [
+    any,
+    anybtc,
+    anyfsn,
+    anyusdt
+  ]
+
+  const [ coordinates, setCoordinates ] = useState(() => {
+    return CHAINS.map(() => {
+      return {
+        x: 0,
+        y: 0,
+        centreX: 0,
+        centreY: 0
+      }
+    });
+  });
+  const [ payload, setPayload ] = useState({
+    from: CHAINS[0],
+    to: CHAINS[0],
+    token: TOKENS[0],
+    amount: 0
+  });
+
+
+
+  const randomTransfer = () => {
+    setInterval(() => {
+      let from = Math.floor(Math.random() * N);
+      let to = Math.floor(Math.random() * N);
+      let token = TOKENS[Math.floor(Math.random() * TOKENS.length)];
+      let amount = 0;
+      while(from === to) {
+        from = Math.floor(Math.random() * N);
+      }
+      
+      setPayload({from, to, token, amount});
+    }, 5000);
+  }
+
+
+
   useEffect(() => {
-    const amount = 8;
-    const [ coordinates, R, r ] = drawCircles(300, amount, 0.4);
-    setBridgeTarget((R + r / 2) + 5);
-    setLayout(
-      <Chains size={2 * (R + r)}>
-        <Payload initial={{opacity: 0}} animate={controls} transition={{ease: "easeOut", duration: 2.5}} />
-        <Bridge top={R + r / 2} left={R + r / 2} />
-        {chains.map((chain, index) => {
-          return (
-            <Crypto
-              top={coordinates[index].y}
-              left={coordinates[index].x}
-              r={2 * r}
-              onClick={() => {
-                const randomChain = Math.floor(Math.random() * amount);
-                setNewTransfer(index, randomChain);
-              }}>
-              <Icon src={chain} r={2 * r}/>
-            </Crypto>
-          );
-        })}
-      </Chains>
-    );
-  }, []);
+    if(coordinates[payload.from] && coordinates[payload.to] && payload.token) {
+      controls.start({
+        x: [ coordinates[payload.from].centreX, BRIDGE, coordinates[payload.to].centreX ],
+        y: [ coordinates[payload.from].centreY, BRIDGE, coordinates[payload.to].centreY ],
+        opacity: [ 0, 1, 0 ]
+      });
+    }
+  }, [ payload ]);
 
-  const drawCircles = (R, n, resize) => {
-    let coordinates = [];
-    let makeTargets = [];
-    let r = Math.PI * R / n * resize;
 
-    for(let ii = 0; ii < n; ii++) {
-      let theta = ii * 2 * Math.PI / n;
-      let x = R * Math.sin(theta) - r;
-      let y = R * Math.cos(theta) + r;
 
-      let X = R + r + x;
-      let Y = R + r - y;
+  // On Page Load ...
+  useEffect(() => {
+    let loadCoordinates = [];
 
-      coordinates.push({
+    for(let ii = 0; ii < N; ii++) {
+      let theta = ii * 2 * Math.PI / N;
+
+      let X = R + r + (R * Math.sin(theta) - r);
+      let Y = R + r - (R * Math.cos(theta) + r);
+
+      loadCoordinates.push({
         x: X,
         y: Y,
-      });
-
-      makeTargets.push({
-        x: X + r / 2,
-        y: Y + r / 2
+        centreX: X + r / 2,
+        centreY: Y + r / 2
       });
     }
 
-    setTargets(makeTargets);
+    setCoordinates(loadCoordinates);
 
-    return [ coordinates, R, r ];
-  }
-
-  const setNewTransfer = (from, to) => {
-    controls.start({
-      x: [ targets[from].x, bridgeTarget, targets[to].x ],
-      y: [ targets[from].y, bridgeTarget, targets[to].y ],
-      opacity: [ 0, 1, 0 ]
-    });
-  }
+  }, []);
 
 
   const controls = useAnimation();
 
   return (
     <ChainsContainer>
-      {layout}
+
+      <Chains size={2 * (R + r)}>
+
+        <Bridge top={R + r / 2} left={R + r / 2} src={anyswap} />
+
+        {
+          CHAINS.map((chain, index) => {
+            return (
+              <Crypto top={coordinates[index].y} left={coordinates[index].x} r={2 * r}>
+                <Icon src={chain} r={2 * r}/>
+              </Crypto>
+            );
+          })
+        }
+
+        <Payload
+          initial={{opacity: 0}}
+          animate={controls}
+          transition={{ease: "easeOut", duration: 5}}
+          src={payload.token}
+        />
+
+      </Chains>
+      <button onClick={() => randomTransfer()} style={{width: "50px", height: "50px"}} />
+
     </ChainsContainer>
   );
 }
